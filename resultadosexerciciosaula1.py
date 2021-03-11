cd06058a567f2ab8ebc644b200d256db66ef6cb8
LucasEvo <42527886+LucasEvo@users.noreply.github.com>
2021-03-11 16:13:08 -0300

    Add files via upload
    Exercícios e resultados da primeira aula de Python

---
+++ resultadosexerciciosaula1.py
@@ -0,0 +1,66 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio1aula1.py =========
+Qual seu nome?Marcos
+Olá Marcos !  Prazer em te conhecer!
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio2aula1.py =========
+Dia = 6
+Mês = Janeiro
+Ano = 1996
+Você nasceu no dia 6 de  Janeiro de  1996 . Correto?
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio2aula1.py =========
+Dia =6
+Mês =janeiro
+Ano =1996
+Você nasceu no dia 6 de janeiro de 1996 . Correto?
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =6
+Número 2 =3
+63
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =6
+Número 2 =3
+6 3
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =1
+Número 2 =1
+numero1numero2
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =1
+Número 2 =1
+Traceback (most recent call last):
+  File "C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py", line 3, in <module>
+    print [(numero1)+(numero2)]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =1
+Número 2 =1
+1
+Traceback (most recent call last):
+  File "C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py", line 3, in <module>
+    print (numero1)+(numero2)
+TypeError: unsupported operand type(s) for +: 'NoneType' and 'str'
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =6
+Número 2 =3
+A soma é igual a 63
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =6
+Número 2 =3
+A soma é igual a 63
+>>> 
+========= RESTART: C:/Users/marco/Documents/ScriptsPython/scriptexercicio3aula1.py =========
+Número 1 =6
+Número 2 =3
+A soma é igual a 9
+>>> 
---
+++ scriptexercicio3aula1.py
@@ -0,0 +1,8 @@
+a_str = input('Número 1 =')
+b_str = input('Número 2 =')
+a_int = int(a_str)
+b_int = int(b_str)
+soma = a_int+b_int
+print ('A soma é igual a',soma)
+
+
